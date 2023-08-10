09bc05fba9cf586e8b79983c0cd719246e0028fa
Kamila Matejczuk <kamila.mate82@gmail.com>
2023-08-10 10:32:14 +0000

    Add new folders

--- montessori/settings.py
+++ montessori/settings.py
@@ -32,8 +32,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["monessori.herokuapp.com", "localhost"]
-
+ALLOWED_HOSTS = ['montessori-parenting-ed9d8dded804.herokuapp.com'
+                 'localhost']
 
 # Application definition
 
