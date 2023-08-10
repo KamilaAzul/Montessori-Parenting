ca729bc5b01b0b446861bff96d6bc611d3dfca54
Kamila Matejczuk <kamila.mate82@gmail.com>
2023-08-10 11:32:22 +0000

    Update settings.py

--- montessori/settings.py
+++ montessori/settings.py
@@ -32,8 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['montessori-parenting-ed9d8dded804.herokuapp.com'
-                 'localhost']
+ALLOWED_HOSTS = ['montessori-parenting-ed9d8dded804.herokuapp.com', 'localhost']
 
 # Application definition
 
