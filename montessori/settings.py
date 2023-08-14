7a83ed606302c40922393ea37e18fd9e1bd5e771
Kamila Matejczuk <kamila.mate82@gmail.com>
2023-08-14 08:50:05 +0000

    Change dubus to flase in setting.py

--- montessori/settings.py
+++ montessori/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['montessori-parenting-ed9d8dded804.herokuapp.com', 'localhost',
                  '8000-kamilaazul-montessoripa-21c4fivnefm.ws-eu103.gitpod.io']
